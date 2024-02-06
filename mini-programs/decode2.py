7a58ab8ed4148b0a540747e74d299b55df46ad19
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-02-06 11:37:12 +0100

    Decoder update

--- mini-programs/decode2.py
+++ mini-programs/decode2.py
@@ -2,31 +2,52 @@
   Transaction Decoder Module
 """
 
-trx_h = input("Raw Transaction in Hex: ")
+trx_h = "020000000001010ccc140e766b5dbc884ea2d780c5e91e4eb77597ae64288a42575228b79e234900000000000000000002bd37060000000000225120245091249f4f29d30820e5f36e1e5d477dc3386144220bd6f35839e94de4b9cae81c00000000000016001416d31d7632aa17b3b316b813c0a3177f5b6150200140838a1f0f1ee607b54abf0a3f55792f6f8d09c3eb7a9fa46cd4976f2137ca2e3f4a901e314e1b827c3332d7e1865ffe1d7ff5f5d7576a9000f354487a09de44cd00000000"
+print("Raw Transaction in Hex:", trx_h, end="\n\n")
 trx_bytes = bytes.fromhex(trx_h)
-i = 0
+i = 4
 
-version_bytes = trx_bytes[0:4]
+# Version
+version_bytes = trx_bytes[0:i]
 version = int(version_bytes[::-1].hex(), 16)
 print("Version : {}\n".format(version))
 i = 4
 
-if trx_bytes[4] == 00:
-    marker_bytes = 00
-    flag_bytes = trx_bytes[5]
-    i = 6
+# Marker and Flag
+if trx_bytes[i] == 00:
+    marker_bytes = trx_bytes[i]
+    i += 1
+    flag_bytes = trx_bytes[i]
+    i += 1
 
-print("Inputs :")
+# Inputs
+print("-----Inputs-----")
 num_input = trx_bytes[i]
-print("Number of input", num_input)
+print("Number of input :", num_input)
 for j in range(num_input):
     print("Input_{}".format(j))
     i += 1
     inputjtrxid = trx_bytes[i:i + 32][::-1].hex()
-    print("Input TRX ID :", inputjtrxid)
-
+    print("Input Trx ID :", inputjtrxid)
+    i += 41
 print()
 
-print("Outputs...\n")
+# Outputs
+print("-----Outputs-----")
+num_output = trx_bytes[i]
+print("Number of output :", num_output)
+for k in range(num_output):
+    print("Output_{}".format(k))
+    i += 1
+    amountoutputk = int(trx_bytes[i:i+8][::-1].hex(), 16)
+    print("Amount Output_{} : {}".format(k, amountoutputk))
+    i += 8
+    lockscriptsize = trx_bytes[i]
+    i += 1
+    lockscript = trx_bytes[i:i+lockscriptsize].hex()
+    i += lockscriptsize - 1
+    print("Output_{} Script : {}".format(k, lockscript))
+print()
 
-print("Locktime", trx_bytes[189:193].hex())
+#Locktime is the last 4 Bytes
+print("Locktime", trx_bytes[-4:].hex())
