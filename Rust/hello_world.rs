ec87716baf5e44f487c76d6d33bd532a1cb3d67b
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-11-20 00:35:37 +0100

    trying mutable variables

--- Rust/hello_world.rs
+++ Rust/hello_world.rs
@@ -7,4 +7,13 @@ fn main() {
     let width = message.chars().count();
     let mut writer = BufWriter::new(stdout.lock());
     say(&message, width, &mut writer).unwrap();
+    let mut my_name = "Alphonse";
+    let my_age: i32 = 26;
+    println!("My name is {my_name} \
+        and I am {my_age}!"
+    );
+    my_name = "Ferris";
+    println!("My name is {my_name} and I \
+        am {my_age}!"
+    );
 }
