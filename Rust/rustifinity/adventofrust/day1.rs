f87b5d288df92e88c79e9165838a6fc938c58a66
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-12-01 23:20:12 +0100

    advent of rust day 1 submission

---
+++ Rust/rustifinity/adventofrust/day1.rs
@@ -0,0 +1,10 @@
+pub fn main() {
+    let gift_message = String::from("Merry Christmas! Enjoy your gift!");
+    attach_message_to_present(gift_message.clone());
+
+    println!("{}", gift_message);
+}
+
+pub fn attach_message_to_present(message: String) {
+    println!("The present now has this message: {}", message);
+}
