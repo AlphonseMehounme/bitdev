53c5ea65b6351fab419266d685d2748c86556113
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-12-05 19:52:51 +0100

    add weekday-from-number challenge

---
+++ Rust/rustifinity/challenges/weekday-from-number.rs
@@ -0,0 +1,13 @@
+pub fn weekday_from_number(day: u8) -> &'static str {
+    // TODO: Implement the function here
+    match day {
+        1 => "Monday",
+        2 => "Tuesday",
+        3 => "Wednesday",
+        4 => "Thursday",
+        5 => "Friday",
+	6 => "Saturday",
+	7 => "Sunday",
+	_ => "Invalid day number"
+    }
+}
