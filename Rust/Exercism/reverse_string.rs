fa33aefdcd8e32e89bea6946405f67a25969d8d3
Alphonse Mehounme <52801872+AlphonseMehounme@users.noreply.github.com>
2025-01-02 23:12:11 +0100

    add exercism rust second challenge result

---
+++ Rust/Exercism/reverse_string.rs
@@ -0,0 +1,15 @@
+pub fn reverse(input: &str) -> String {
+    input.chars().rev().collect()
+}
+
+
+
+
+
+[package]
+edition = "2021"
+name = "reverse_string"
+version = "1.2.0"
+
+[features]
+grapheme = []
